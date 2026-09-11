0a86928fe18fb9f2c50224bbd1192e379a069033
ArtemXtheArtiX <artemshmulev251120071@gmail.com>
2026-09-11 20:27:53 +0700

    Update GraffityClient.java

--- src/client/java/ru/yourname/client/GraffityClient.java
+++ src/client/java/ru/yourname/client/GraffityClient.java
@@ -1,10 +1,32 @@
 package ru.yourname.client;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderEvents;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import org.lwjgl.glfw.GLFW;
+import ru.yourname.GraffitiMod;
 
-public class GraffityClient implements ClientModInitializer {
+public class GraffitiClient implements ClientModInitializer {
+	public static KeyBinding keyOpenManager;
+
 	@Override
 	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+		keyOpenManager = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+			"key.graffity.open_manager", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_G, "category.graffity"
+		));
+
+		WorldRenderEvents.LAST.register(GraffitiRenderer::onRenderWorldLast);
+
+		ClientTickEvents.END_CLIENT_TICK.register(client -> {
+			if (client.world != null) {
+				GraffitiManager.tick();
+			}
+			if (client.currentScreen == null && keyOpenManager.wasPressed()) {
+				client.setScreen(new GuiGraffitiManager());
+			}
+		});
 	}
 }
