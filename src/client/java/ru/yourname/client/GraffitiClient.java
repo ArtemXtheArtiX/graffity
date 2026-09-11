ed3476421c6253c79aced1c9b9b76a2dd8e90ec6
ArtemXtheArtiX <artemshmulev251120071@gmail.com>
2026-09-11 22:04:35 +0700

    Update GraffitiClient.java

--- src/client/java/ru/yourname/client/GraffitiClient.java
+++ src/client/java/ru/yourname/client/GraffitiClient.java
@@ -3,7 +3,7 @@ package ru.yourname.client;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderEvents;
+import net.fabricmc.fabric.api.client.rendering.v1.world.WorldRenderEvents;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 import net.minecraft.util.Identifier;
@@ -19,7 +19,6 @@ public class GraffitiClient implements ClientModInitializer {
             "key.graffity.open_manager", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_G, CATEGORY
         ));
 
-        // Возвращаем стабильную регистрацию события рендера
         WorldRenderEvents.LAST.register(GraffitiRenderer::onRenderWorldLast);
 
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
